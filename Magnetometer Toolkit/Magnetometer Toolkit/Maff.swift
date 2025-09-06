4ad20d6dad8c4202d977fca75c2b1a9bf0d87d6f
PlutoSpaceLab <plutospacelab@gmail.com>
2025-09-06 16:22:06 -0700

    Update UI

--- Magnetometer Toolkit/Magnetometer Toolkit/Maff.swift
+++ Magnetometer Toolkit/Magnetometer Toolkit/Maff.swift
@@ -1,8 +1,20 @@
-//
 //  Maff.swift
 //  Magnetometer Toolkit
-//
-//  Created by Simon Chu on 9/2/25.
-//
 
 import Foundation
+
+enum Maff {
+    /// Reusable, cached UTC formatter (DateFormatter is not thread-safe;
+    /// we only use it from the main/UI thread in this app).
+    static let utcFormatter: DateFormatter = {
+        let f = DateFormatter()
+        f.timeZone = TimeZone(secondsFromGMT: 0)
+        f.dateFormat = "yyyy-MM-dd HH:mm:ss 'UTC'"
+        return f
+    }()
+
+    /// Convenience helper
+    static func utcString(_ date: Date) -> String {
+        utcFormatter.string(from: date)
+    }
+}
